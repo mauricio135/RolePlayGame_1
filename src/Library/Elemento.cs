1e09444fdf83d90a07c11fe2199c26592d3b52d6
Nahuel Costa <nahuelcostadalto@hotmail.com>
2020-04-22 02:01:02 -0300

    Personaje,Test,calculodedaños

---
+++ src/Library/Elemento.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+
+namespace RolePlayGame_1.Library
+{
+    public class Elemento
+    {
+        private string _nombre;
+        public string Nombre
+        {
+            get {return _nombre;}
+            set {_nombre = value;}
+        }
+        private int _ataque;
+        public int Ataque
+        {
+            get { return _ataque; }
+            set { _ataque = value; }
+        }
+        private int _defensa;
+        public int Defensa
+        {
+            get { return _defensa; }
+            set { _defensa = value; }
+        }
+
+        public Elemento (string nombre, int ataque, int defensa)
+        {
+            this.Nombre = nombre;
+            this.Ataque = ataque;
+            this.Defensa = defensa;
+        }
+
+
+    }
+    
+}
---
+++ src/Test/Library.Test/ElfoTest.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+using RolePlayGame_1.Library;
+
+namespace Library.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CalculoDeDañoCorrecto()
+
+        {   Elfo Legolas=new Elfo("Legolas");  
+            Legolas.Vida-=35;
+            Assert.AreEqual(65,Legolas.Vida) ;
+            
+        }
+        [Test]
+        public void DefensaTotalCorrecta()
+        {
+            Elfo Dobby =new Elfo("Dobby");
+            Elemento item1 = new Elemento("calsetín",10,0);
+            Elemento item2 = new Elemento ("Daga de Belatrix",0,50);
+            Dobby.AgregarElemento(item1);
+            Dobby.AgregarElemento(item2);
+
+            Assert.AreEqual(50,Dobby.CalcularDefensaTotal());
+        
+        }
+        [Test]
+        public void CalculoDeSumaDeAtaque()
+        {
+            Elfo elfo = new Elfo("elfo");
+            Elemento item1= new Elemento("cerveza",10,-3);
+            Elemento item2 = new Elemento ("daga",20,0);
+            elfo.AgregarElemento(item1);
+            elfo.AgregarElemento(item2);
+            Assert.AreEqual(30,elfo.CalcularAtaqueTotal());
+        }
+    }
+}
